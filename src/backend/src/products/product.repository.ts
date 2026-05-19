baa93e9ba63231035ea7a9f9651aab0b5fbcb538
teakayy14 <kiennh1325@gmail.com>
2026-05-19 23:28:52 +0700

    Product

--- src/backend/src/products/product.repository.ts
+++ src/backend/src/products/product.repository.ts
@@ -4,4 +4,10 @@ export const PRODUCT_REPOSITORY = 'PRODUCT_REPOSITORY';
 
 export interface ProductRepository {
   findOne(productID: number): Promise<Product | null>;
+  findByBarcode(barcode: string): Promise<Product | null>;
+  create(dto: any): Product;
+  save(product: Product): Promise<Product>;
+  checkInOrders(productID: number): Promise<boolean>;
+  delete(productID: number): Promise<void>;
+  getDailyDeletionCount(): Promise<number>;
 }
